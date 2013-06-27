24628b8cb6e2f1ae7779a69bd31e5ccfdb4bd9d5
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-06-27 08:16:45 -0400

    added angular libraries and basic functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 MusicApp::Application.routes.draw do
-  root to: "welcome#index"
+  root to: "sessions#new"
   get '/login', to: 'sessions#new', as: 'login'
   match '/auth/:provider/callback', to: 'sessions#create', :via => [:get, :post]
   match "/logout", to: "sessions#destroy", :via => [:get, :post]
