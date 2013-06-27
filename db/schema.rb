07650cf8e746b59113c4cea9c71e85bf6bc016b1
Basil Siddiqui <basil.siddiqui@gmail.com>
2013-06-27 19:08:57 -0400

    removed weird shit from schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130623204712) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "authentications", force: true do |t|
     t.string   "provider",            null: false
     t.string   "uid",                 null: false
