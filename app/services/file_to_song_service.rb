de307dc1184c092db5bfba5b60e971f8042fc6d4
Matt Nguyen <mnguyen916@gmail.com>
2013-08-04 13:39:20 -0700

    Mark songs as removed when they are removed from db

--- app/services/file_to_song_service.rb
+++ app/services/file_to_song_service.rb
@@ -1,7 +1,8 @@
 class FileToSongService
   def initialize(file_list, user)
-    @file_list = file_list
-    @user      = user
+    @file_list  = file_list
+    @user       = user
+    @start_time = Time.now.utc
   end
 
   def convert_and_save
@@ -9,10 +10,17 @@ class FileToSongService
   end
 
   def find_or_create_song(song)
-    Song.where(user_id: @user.id, provider: "dropbox", path: song['path']).first_or_initialize do |s|
+    Song.where(user_id: @user.id, provider: "dropbox", path: song['path']).first_or_initialize.tap do |s|
       s.name       = song['path'][1..-1].gsub('_',' ')
       s.properties = song
-      s.save! if s.changed?
+      s.removed_at = nil
+      s.changed? ? s.save! : s.touch
+    end
+  end
+
+  def update_removed_dropbox_songs
+    Song.where("user_id = ? AND updated_at < ?", @user.id, @start_time).map do |song|
+      song.removed_from_dropbox!
     end
   end
 end
