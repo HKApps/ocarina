ff13ab3da0e873a55c401e73fd39f47107b2c9a9
Matt Nguyen <mnguyen916@gmail.com>
2013-08-05 23:49:23 -0700

    Fix redis connection issue in prod

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,5 @@ module Ocarina
     config.assets.precompile += %w(*.png *.jpg *jpeg *.gif)
 
     config.autoload_paths += %W( #{config.root}/lib )
-
-    config.cache_store = :redis_store
   end
 end
