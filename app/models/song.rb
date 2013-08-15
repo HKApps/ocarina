328d75b67a6336e8bf1f1a1f46232604fe0137f2
Matt Nguyen <mnguyen916@gmail.com>
2013-08-14 21:11:31 -0700

    Create playlist_songs from soundcloud objects
    https://trello.com/c/tGcg8Nbe/152-3-update-api-endpoint-for-add-songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,6 +6,15 @@ class Song < ActiveRecord::Base
   validates :path, presence: true
   validates :name, presence: true
 
+  def self.find_or_initialize_from_soundcloud(sc, user_id)
+    Song.where(user_id: user_id, provider: 'soundcloud', path: sc['uri']).first_or_initialize.tap do |s|
+      s.name       = sc['title']
+      s.properties = sc
+      s.removed_at = nil
+      s.changed? ? s.save! : s.touch
+    end
+  end
+
   def removed_from_dropbox!
     self.removed_at = Time.now
     self.save!
--- app/services/add_playlist_song_to_playlist_service.rb
+++ app/services/add_playlist_song_to_playlist_service.rb
@@ -1,19 +1,29 @@
 class AddPlaylistSongToPlaylistService
-  def self.initialize_from_params(params)
-    playlist_id = params.delete(:id)       { raise "Required param: id" }
-    song_ids    = params.delete(:song_ids) { raise "Required param: song_ids" }
+  def self.initialize_from_params(params, user_id)
+    playlist_id = params.delete(:id)         { raise "Required param: id" }
+    dropbox     = params.delete(:dropbox)    { raise "Required param: dropbox" }
+    soundcloud  = params.delete(:soundcloud) { raise "Required param: soundcloud" }
 
-    new(song_ids.split(','), playlist_id)
+    new(dropbox, soundcloud, playlist_id, user_id)
   end
 
-  def initialize(song_ids, playlist_id)
-    @song_ids = song_ids
+  def initialize(dropbox, soundcloud, playlist_id, user_id)
+    @dropbox     = dropbox
+    @soundcloud  = soundcloud
     @playlist_id = playlist_id
+    @user_id     = user_id
+    @playlist_songs = []
   end
 
   def create
+    create_from_dropbox   if @dropbox
+    create_from_souncloud if @soundcloud
+    @playlist_songs
+  end
+
+  def create_from_dropbox
     ActiveRecord::Base.transaction do
-      songs.map do |song|
+      dropbox_songs.map do |song|
         playlist_song = song.playlist_songs.create do |ps|
           ps.path        = song.path
           ps.song_name   = song.name
@@ -21,13 +31,35 @@ class AddPlaylistSongToPlaylistService
           ps.playlist_id = @playlist_id
         end.attributes
         playlist_song["current_user_vote_decision"] = 0
-        playlist_song
+        @playlist_songs << playlist_song
       end
     end
   end
 
-  def songs
-    @songs ||= Song.where(id: @song_ids)
+  def create_from_souncloud
+    ActiveRecord::Base.transaction do
+      @soundcloud.map do |sc|
+        song = Song.find_or_initialize_from_soundcloud(sc, @user_id)
+
+        playlist_song = song.playlist_songs.create do |ps|
+          ps.path        = song.path
+          ps.song_name   = song.name
+          ps.provider    = song.provider
+          ps.playlist_id = @playlist_id
+          ps.media_url   = "#{song.path}/stream?client_id = 3d6e76640c62f42c02cb78d2c53d0db9"
+        end.attributes
+        playlist_song["current_user_vote_decision"] = 0
+        @playlist_songs << playlist_song
+      end
+    end
+  end
+
+  def soundcloud_ids
+    @soundcloud.map { |song| song["id"] }
+  end
+
+  def dropbox_songs
+    @dropbox_songs ||= Song.where(id: @dropbox)
   end
 
   def songs_with_media_urls(dropbox_client)
