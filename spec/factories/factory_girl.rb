e41ab1e55258797e8f90b5e0fd3d59f49d66f325
Matt Nguyen <mnguyen916@gmail.com>
2013-09-03 22:32:10 -0700

    Create specs for song model

--- spec/factories/factory_girl.rb
+++ spec/factories/factory_girl.rb
@@ -24,4 +24,23 @@ FactoryGirl.define do
     association :user, strategy: :build
   end
 
+  factory :song do
+    sequence :name do |n|
+      "playlist_#{n}"
+    end
+    provider "some_provider"
+    path     "/path/to/song"
+    association :user
+
+    trait :song_from_dropbox do
+      provider "dropbox"
+      path     "/path/to/dropbox/song"
+    end
+
+    trait :song_from_soundcloud do
+      provider "soundcloud"
+      path     "/path/to/soundcloud/song"
+    end
+  end
+
 end
