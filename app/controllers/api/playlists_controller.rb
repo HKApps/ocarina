433033b0c3fe3c9ef4892ee8cc618d932bc7bec4
Abhishek Gadiraju <abhishek.gadiraju@gmail.com>
2013-09-08 01:26:17 -0400

    convert to coords 1.0

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -12,6 +12,12 @@ class Api::PlaylistsController < ApiController
 
   def create
     @playlist = current_user.playlists.build(playlist_params)
+    if !@playlist.venue.latitude && @playlist.location
+        coords = convert_location_to_coords(@playlist.location)
+        @playlist.venue.latitude = coords["latitude"]
+        @playlist.venue.longitude = coords["longitude"]
+    end
+
     if @playlist.save
       respond_with @playlist, status: 201
     else
@@ -75,6 +81,15 @@ class Api::PlaylistsController < ApiController
     respond_to do |format|
       format.json { head :ok }
     end
+  end
+
+  def convert_location_to_coords(location)
+    geocoder_object = Geocoder.search(location)
+    # next if !geocoder_object
+    latitude = geocoder_object[0].lat.to_f
+    longitude = geocoder_object[0].lng.to_f
+
+    respond_with [latitude, longitude], status: 201
   end
 
   def push_guest(guest, playlist_id)
