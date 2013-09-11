066592d64179f690d4fe1bd88d0ab2cd4fe2804d
Matt Nguyen <mnguyen916@gmail.com>
2013-09-11 00:13:21 -0700

    Update playlist api

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -13,7 +13,7 @@ class Api::PlaylistsController < ApiController
   end
 
   def create
-    @playlist = current_user.playlists.build(playlist_params)
+    @playlist = user.playlists.build(playlist_params)
     if @playlist.save
       respond_with @playlist, status: 201
     else
@@ -25,8 +25,8 @@ class Api::PlaylistsController < ApiController
     @playlist = Playlist.where(id: params[:id]).first
     if @playlist
       if @playlist.password == params[:password]
-        JoinPlaylistWorker.new.async.perform(@playlist.id, current_user.id)
-        push_guest(current_user, params[:id])
+        JoinPlaylistWorker.new.async.perform(@playlist.id, params[:user_id])
+        push_guest(params[:user_id], params[:id])
         render "api/playlists/join", status: 201
       else
         render "api/playlists/join", json: {error: "wrong password", status: 401 }
